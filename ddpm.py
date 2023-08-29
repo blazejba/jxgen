f78f867fbd1b185786a672c612e12e4cf5181ff2
Blazej Banaszewski <blazej@banaszewski.pl>
2023-08-29 22:07:36 +0200

    add linear diffusion schedule

--- ddpm.py
+++ ddpm.py
@@ -3,19 +3,39 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 
 
-if __name__ == '__main__':
-    # Download the dataset
-    ds, ds_info = tfds.load('oxford_flowers102', split='train', with_info=True)
+def linear_diffusion_schedule(diffusion_times: int,
+                              max_rate: float = 0.02,
+                              min_rate: float = 0.0001):
+    """Linear schedule for diffusion rate."""
+    betas       = min_rate + jnp.array(diffusion_times) * (max_rate - min_rate)
+    alphas      = 1 - betas
+    alpha_bars  = tf.math.cumprod(alphas)
 
-    # Prefetching and batching
-    ds = ds.batch(32).prefetch(tf.data.experimental.AUTOTUNE)
+    signal_rates =     alpha_bars
+    noise_rates  = 1 - alpha_bars
+    return noise_rates, signal_rates
+        
+
+def load_dataset():
+    ds, _ = tfds.load('oxford_flowers102', split='train', with_info=True)
+    ds    = ds.map(lambda x: (tf.image.resize(x['image'], (64, 64)) / 255.0, x['label']))
+    ds    = ds.batch(32).prefetch(tf.data.experimental.AUTOTUNE)
+    return ds
+
+
+if __name__ == '__main__':
+    ds = load_dataset()
+
+    T = 1000
+    diffusion_times = [x/T for x in range(T)]
+    linear_noise_rates, linear_signal_rates = linear_diffusion_schedule(
+        diffusion_times
+    )
 
     for batch in ds:
         images, labels = batch
-
-        # Convert to JAX DeviceArray
-        images = jnp.array(images)
-        labels = jnp.array(labels)
+        images, labels = jnp.array(images), jnp.array(labels)
         
         print(images.shape)
+        print(labels.shape)
         exit()
